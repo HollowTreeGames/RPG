1490eca8caab3045ddcc46fab89e836a419ad45a
pixel-prism <artist@cvilbrandt.com>
2018-08-07 17:46:34 -0700

    NPC Walking, New TIlemap, Portraits
    Added portrait placeholder, and randomized NPC walking. Animations still don't work.

--- Assets/Scripts/Bases/NPC.cs
+++ Assets/Scripts/Bases/NPC.cs
@@ -6,11 +6,26 @@ using Enums;
 public class NPC : Talkable
 {
     
-    private Animator animator;
+    public Animator animator;
     private BoxCollider2D boxCollider2D;
+    private Rigidbody2D myRigidbody;
+    private System.Random random = new System.Random();
+
+    public float moveSpeed;
+    private bool walking;
+    public float timeBetweenMove;
+    private float timeBetweenMoveCounter;
+    public float timeToMove;
+    private float timeToMoveCounter;
+    private float chooseDirection;
+    private Vector3 moveDirection;
 
     protected override void Start()
     {
+        timeBetweenMoveCounter = Random.Range (timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+        timeToMoveCounter = timeToMove;
+        myRigidbody = GetComponent<Rigidbody2D>();
+
         base.Start();
         animator = GetComponent<Animator>();
         boxCollider2D = GetComponent<BoxCollider2D>();
@@ -21,6 +36,52 @@ public class NPC : Talkable
         Player player = FindObjectOfType<Player>();
         TurnToPlayer(player.transform.position);
         base.Interact();
+    }
+
+    public void Update()
+    {
+        if (walking)
+        {
+            timeToMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = moveDirection;
+
+            if (timeToMoveCounter < 0f)
+            {
+                walking = false;
+                timeBetweenMoveCounter = timeBetweenMove;
+            }
+        } else
+        {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = Vector2.zero;
+
+            if (timeBetweenMoveCounter < 0f)
+            {
+                walking = true; 
+                timeToMoveCounter = timeToMove;
+
+                chooseDirection = Random.Range(-1, 2);
+                if (chooseDirection > 0) {
+                    int NPCY = random.Next(-1, 2);
+                    moveDirection = new Vector3(0, NPCY * moveSpeed, 0f);
+                    Debug.Log("ydirection" + NPCY);
+                    animator.SetFloat("moveX", 0);
+                    animator.SetFloat("moveY", NPCY);
+                    animator.SetFloat("lastMoveX", 0);
+                    animator.SetFloat("lastMoveY", NPCY);
+                } else
+                {
+                    int NPCX = random.Next(-1, 2);
+                    moveDirection = new Vector3(NPCX * moveSpeed, 0, 0f);
+                    Debug.Log("xdirection" + NPCX);
+                    animator.SetFloat("moveX", NPCX);
+                    animator.SetFloat("moveY", 0);
+                    animator.SetFloat("lastMoveX", NPCX);
+                    animator.SetFloat("lastMoveY", 0);
+                }
+                
+            }
+        }
     }
 
     public void TurnToPlayer(Vector3 playerPosition)
