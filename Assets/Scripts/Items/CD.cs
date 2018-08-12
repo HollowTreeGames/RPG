02d0edab72530bc26dc4a0ff23c304ee81e509a8
pixel-prism <artist@cvilbrandt.com>
2018-08-11 20:42:14 -0700

    Changed CD words

--- Assets/Scripts/Items/CD.cs
+++ Assets/Scripts/Items/CD.cs
@@ -6,26 +6,36 @@ using MyDialogue;
 
 public class CD : Item {
 
-    private GameState gameState;
-
     protected override void Start()
     {
         base.Start();
         itemName = "CD";
         gameState = FindObjectOfType<GameState>();
+
+        DLine[] pickUp =
+        {
+            new DLine("Belfry", "Default", "Ooo, shiny!"),
+            new DLine("Belfry", "Happy", "'America Online Installation CD.'"),
+            new DLine("Belfry", "Default", "The colors are so pretty on this..."),
+            new DLine("Belfry", "Happy", "This seems cool!")
+        };
+        this.pickUp = pickUp;
+
+        DLine[] handsFull =
+        {
+            new DLine("Belfry", "Sad", "That looks delicate. I probably need both paws to hold it!")
+        };
+        this.handsFull = handsFull;
+
+        DLine[] defaultDialogue =
+        {
+            new DLine("Belfry", "Happy", "It's a shiny...circle...disk thingy!!")
+        };
+        this.defaultDialogue = defaultDialogue;
     }
 
-    public override void Interact()
+    protected override bool CheckForPickup()
     {
-        if (gameState.findCD == QuestState.InProgress)
-        {
-            base.Interact();
-        }
-        else
-        {
-            dialogueManager.StartDialogue(new DLine("Belfry", "Default", 
-                "America Online Installation CD-ROM. It's sure shiny! I like the way it catches the light!"
-                ));
-        }
+        return (gameState.findCD == QuestState.InProgress);
     }
 }
