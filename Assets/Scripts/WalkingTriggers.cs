f4a7b748b643a974610d6fb5bdff80305cc3d94d
Marco McCain <marco262@gmail.com>
2018-09-24 10:23:31 -0700

    Bug fix for WalkingTriggers

--- Assets/Scripts/WalkingTriggers.cs
+++ Assets/Scripts/WalkingTriggers.cs
@@ -2,28 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WalkingTriggers : MonoBehaviour {
+public class WalkingTriggers : SpriteParent {
 
     private QuestManager questManager;
     private Quest quest;
-    private bool steppedOn;
+    private DialogueVariableStorage dialogueVariableStorage;
+    private bool steppedOn = false;
 
 	// Use this for initialization
-	void Start () {
+	protected override void Start () {
+        base.Start();
         questManager = FindObjectOfType<QuestManager>();
+        dialogueVariableStorage = FindObjectOfType<DialogueVariableStorage>();
         quest = questManager.FindQuest("Oakewood Stretch Legs");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (quest.IsInProgress() && !steppedOn) {
-            gameObject.SetActive(true);
+            spriteRenderer.enabled = true;
         }
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!quest.IsInProgress() || steppedOn)
+            return;
         steppedOn = true;
+        dialogueVariableStorage.Increment("$oakewood_walking_count");
         gameObject.SetActive(false);
     }
 }
