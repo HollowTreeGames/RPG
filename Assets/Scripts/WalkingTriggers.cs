5bf0b57f932c0a6222bc8f2df3f16eb6d7bc4f2e
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2021-01-10 11:23:09 -0800

    Updated to new version of Unity

--- Assets/Scripts/WalkingTriggers.cs
+++ Assets/Scripts/WalkingTriggers.cs
@@ -14,22 +14,22 @@ public class WalkingTriggers : SpriteParent {
         base.Start();
         questManager = FindObjectOfType<QuestManager>();
         dialogueVariableStorage = FindObjectOfType<DialogueVariableStorage>();
-        quest = questManager.FindQuest("Oakewood Stretch Legs");
+        //quest = questManager.FindQuest("Oakewood Stretch Legs");
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (quest.IsInProgress() && !steppedOn) {
-            spriteRenderer.enabled = true;
-        }
+		//if (quest.IsInProgress() && !steppedOn) {
+  //          spriteRenderer.enabled = true;
+  //      }
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!quest.IsInProgress() || steppedOn)
-            return;
-            steppedOn = true;
-            dialogueVariableStorage.Increment("$oakewood_walking_count");
-            gameObject.SetActive(false);
+        //if (!quest.IsInProgress() || steppedOn)
+        //    return;
+        steppedOn = true;
+        dialogueVariableStorage.Increment("$oakewood_walking_count");
+        gameObject.SetActive(false);
     }
 }
